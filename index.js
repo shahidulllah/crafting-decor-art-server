c75f8e13be6006be75074e96907c3716e4e5d60f
shahidullah177 <mdshahidsumon177@gmail.com>
2024-04-30 04:26:51 +0600

    Some modification

--- index.js
+++ index.js
@@ -36,13 +36,14 @@ async function run() {
       res.send(result);
     })
 
-    //Read item
+    //Read items
     app.get('/userItem', async (req, res) => {
       const cursor = userItemCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
-      
+     
+    //Read Item by Id
     app.get('/userItem/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
@@ -50,7 +51,7 @@ async function run() {
       res.send(result)
   })
 
-    // Update a document
+    // Update Item
     app.put('/userItem/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
